fbb872e714d7d2c9e9a359491141ea5210891b44
larenegade <hausoflarenegade@gmail.com>
2025-11-21 22:59:02 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import { BlockedProvider } from './context/BlockedContext';
 import StaffApp from './apps/StaffApp';
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/*" element={<StaffApp />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <BlockedProvider>
+        <Router>
+          <Routes>
+            <Route path="/*" element={<StaffApp />} />
+          </Routes>
+        </Router>
+      </BlockedProvider>
+    </AuthProvider>
   );
 }
