859682012e0635033b42802ed758af7cfe5bcc8e
larenegade <hausoflarenegade@gmail.com>
2025-11-22 03:50:29 -0500

    Update Settings.js

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,5 +1,5 @@
 import { Container, Typography, TextField, Button, List, ListItem } from '@mui/material';
-import { MenuItem } from '@mui/material';
+import { MenuItem } from '@mui/material';  
 import { Select, FormControl, InputLabel, MenuItem } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { db } from '../firebase';
