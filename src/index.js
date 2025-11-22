65fda03b7400ad2e68d9239e545377071e5ae7f8
larenegade <hausoflarenegade@gmail.com>
2025-11-22 03:33:11 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { AuthProvider } from './context/AuthContext';  // ← THIS LINE WAS MISSING
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AuthProvider content={<App />} />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
